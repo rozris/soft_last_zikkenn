f90fcbcba401529eadcf4f1b9d45072e533c5153
rozris <148305168+rozris@users.noreply.github.com>
2025-12-19 14:59:49 +0900

    Update outchr.s

--- test3_raw/outchr.s
+++ test3_raw/outchr.s
@@ -8,10 +8,10 @@ outbyte:
 o_loop:
 	move.l #SYSCALL_NUM_PUTSTRING, %d0
 	move.l %sp,%a0
- 	add.l #27, %a0 /*ch=%sp + 27 */
+ 	add.l #28, %a0 /*ch=%sp + 27 */
 	move.l (%a0), %d1 /* ch = a0 */
  	move.l %sp, %d2
- 	addi.l #28, %d2 /*p=%sp + 19 */
+ 	addi.l #27, %d2 /*p=%sp + 19 */
  	move.l #1, %d3 /* size = 1 */
  	trap #0
 
