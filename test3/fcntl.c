5911705eca24dbecb7daa0b21bff5880527616ac
rozris <148305168+rozris@users.noreply.github.com>
2026-01-07 06:53:58 +0900

    Update fcntl.c

--- test3/fcntl.c
+++ test3/fcntl.c
@@ -1,12 +1,11 @@
 #include <stdarg.h>
 #include <fcntl.h>
-int fcntl(int fd, int cmd, ...);
- if(cmd == F_GETFL)
- {
- return O_RDWR;
- }
- else
- {
- return 0;
- }
+
+int fcntl(int fd, int cmd, ...) {
+    /*ライブラリ内部でファイル記述子の状態（読み書き可能か等）を確認するために呼ばれる。*/
+    if(cmd == F_GETFL) {
+        return O_RDWR;
+    } else {
+        return 0;
+    }
 }
